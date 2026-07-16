9ef2c33c1745362bc7b4fc2e7607d796ea448f2d
kokueri <fujimoto2092@gmail.com>
2026-07-17 00:00:01 +0900

    キャッシュバスター追加・SVGサイズ属性・ダークモード視認性改善

--- app.js
+++ app.js
@@ -153,9 +153,9 @@ const runName = $('run-name'), runTime = $('run-time'), runCycle = $('run-cycle'
 const stationsEl = $('stations'), pauseBtn = $('pause-btn');
 const ringFg = $('ring-fg');
 const ICONS = {
-  play:  '<svg viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>',
-  pause: '<svg viewBox="0 0 24 24"><path d="M6 5h4v14H6zM14 5h4v14h-4z"/></svg>',
-  replay: '<svg viewBox="0 0 24 24"><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg>',
+  play:  '<svg width="34" height="34" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>',
+  pause: '<svg width="34" height="34" viewBox="0 0 24 24"><path d="M6 5h4v14H6zM14 5h4v14h-4z"/></svg>',
+  replay: '<svg width="34" height="34" viewBox="0 0 24 24"><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg>',
 };
 const RING_C = 2 * Math.PI * 46;
 ringFg.style.strokeDasharray = RING_C;
